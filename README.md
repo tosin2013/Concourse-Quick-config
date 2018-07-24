9d0faa1cbb3d4744247d9b12a06f6ff253c53373
Tosin Akinosho <oluwatosin.akinosho@altoros.com>
2018-07-24 09:17:02 -0400

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Concourse-Quick-config
+# Concourse Quick Config
+
+The following script will automatically configure concourse servers in your network via BOSH.
+
+### Prerequisites
+
+* bosh
+* git
+* bbl
+
+### Installing
+
+* Git clone code to directory
+* update environment variables in concourse-env file.
+* start script
+
+
+
+## Deployment
+
+quickconfigforconcource.sh will not prompt on change of instance type. It will skip any user prompts and just deploy. Script will create a folder under your ~/workspace directory with output.
+
+## Usage
+```
+Tosins-MacBook-Pro:concousedeployments tosinakinosho$ ./quickconfigforconcourse.sh | tee output.log
+Cloning into 'bosh-bootloader'...
+remote: Counting objects: 26734, done.
+remote: Compressing objects: 100% (161/161), done.
+remote: Total 26734 (delta 105), reused 138 (delta 70), pack-reused 26488
+Receiving objects: 100% (26734/26734), 15.81 MiB | 6.12 MiB/s, done.
+Resolving deltas: 100% (18592/18592), done.
+step: generating terraform template
+step: generating terraform variables
+step: terraform init
+step: terraform init
+step: terraform apply
+
+```
+
+## Built With
+
+* [bosh-cli](https://bosh.io/docs/cli-v2/) bosh-cli
+* [bbl](https://github.com/cloudfoundry/bosh-bootloader/) - bosh-bootloader
+
+
+## Authors
+
+* **Tosin Akinosho** - *Initial work* - [Gitlab](https://gitlab.altoros.com/oluwatosin.akinosho)
---
+++ quickconfigforconcourse_vmware.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# This script will autotomaitcally create a concourse server with a custom root disk_size
+
+
+source concourse-env
+
+if [[ ! -d ~/workspace ]]; then
+    mkdir ~/workspace
+fi
+
+#check for debug mode
+if [[ $DEBUG == "TRUE" ]]; then
+	RUNDEBUG="--debug"
+fi
+
+
+CONCOURSEWORKSPACE=concourse_$(date +%Y%m%d_%H%M%S)
+cd ~/workspace/
+mkdir $CONCOURSEWORKSPACE && cd $CONCOURSEWORKSPACE
+
+git clone https://github.com/cloudfoundry/bosh-bootloader.git
+cd bosh-bootloader
+bbl up --lb-type concourse $RUNDEBUG
+
+export external_url="https://$(bbl lbs | awk -F':' '{print $2}' | tr -d ' ')"
+echo "$external_url" || exit 1
+eval "$(bbl print-env)"
+
+bosh upload-stemcell https://bosh.io/d/stemcells/bosh-google-kvm-ubuntu-trusty-go_agent
+
+cat > "update_config.yml" << EOF
+- type: replace
+  path: /vm_types/name=default/cloud_properties?
+  value:
+    machine_type: ${MACHINETYPE}
+    root_disk_size_gb: ${ROOTDISKSIZE}
+    root_disk_type: pd-ssd
+EOF
+
+## automatically update the colud config env to preset disk size
+bosh cloud-config > cloud.yml || exit 1
+bosh interpolate cloud.yml --ops-file update_config.yml > new_cloud_config.yml
+bosh update-cloud-config new_cloud_config.yml -n
+
+
+## GO to consourse bosh deployment path
+git clone https://github.com/concourse/concourse-bosh-deployment.git
+cd concourse-bosh-deployment/cluster
+
+## start concourse deployment
+bosh deploy -d concourse concourse.yml \
+  -l ../versions.yml \
+  --vars-store cluster-creds.yml \
+  -o operations/no-auth.yml \
+  -o operations/privileged-http.yml \
+  -o operations/privileged-https.yml \
+  -o operations/tls.yml \
+  -o operations/tls-vars.yml \
+  -o operations/web-network-extension.yml \
+  --var network_name=default \
+  --var external_url=$external_url \
+  --var web_vm_type=default \
+  --var db_vm_type=default \
+  --var db_persistent_disk_type=10GB \
+  --var worker_vm_type=default \
+  --var deployment_name=concourse \
+  --var web_network_name=private \
+  --var web_network_vm_extension=lb -n
+
+sleep 15s
+
+bosh vms
+
+echo "OUTPUT PATH:"
+echo "cd ../../$(pwd)"
+echo "You may need to export the following into your env"
+echo "export BBL_IAAS=gcp"
+echo 'export BBL_GCP_REGION="us-east1"'
+echo 'export BBL_GCP_SERVICE_ACCOUNT_KEY="'${KEYPATH}'"'
+echo 'eval "$(bbl print-env)"'
+echo "Your Concourse URL: $external_url"
